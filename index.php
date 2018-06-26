a5dd7eb40687cb270cad5a950ae24e93c49dfc37
Bluscream <admin@timo.de.vc>
2018-06-26 23:07:03 +0200

    added level display and bg video

--- index.php
+++ index.php
@@ -12,55 +12,59 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BlackSquad Rank Sync</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style>
-        .table {
-            table-layout: fixed;
-            word-wrap: break-word;
-        }
-    </style>
-  </head>
-  <body style="background-color: #EEE;">
-    <div class="container" style="margin-top: 30px; margin-bottom: 30px; padding-bottom: 10px; background-color: #FFF;">
-		<center><img src="img/BlackSquad.png" />
-		<hr>
-		<?php
-		if(!isset($_SESSION['steamid'])) {
-			echo "<div style='margin: 30px auto; text-align: center;'>Welcome Guest! Please log in!<br>";
-			loginbutton();
-			echo "</div>";
-		}  else {
-			include ('steamauth/userInfo.php');
-			include ('steamauth/userAchievements.php');
-			$level = " Level 0-30";
-			foreach($steamprofile['achievements'] as $value) {
-				if (!$value["achieved"]) continue;
-				switch ($value["name"]) {
-					case "A008_BlackSquad": $level = "Level 100"; break;
-					case "A006_FieldOfficer": $level = "Level 60-99"; break;
-					case "A005_HigherOfficer": $level = "Level 50-59"; break;
-					case "A004_SeniorOfficer": $level = "Level 40-49"; break;
-					case "A003_Officer": $level = "Level 30-39"; break;
-				}
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/index.css">
+		<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+		<style>
+			.table {
+				table-layout: fixed;
+				word-wrap: break-word;
 			}
-			echo $level;
-			logoutbutton();
-		}    
-		?>
-		<hr>
-		</center>
-		<div class="pull-right">
+		</style>
+		<title>BlackSquad Rank Sync</title>
+	</head>
+	<body><!--  style="background-color: #EEE;"-->
+		<video autoplay muted loop id="myVideo">
+			<source src="vid/BSQ_main_25s_0421.mp4" type="video/mp4">
+		</video>
+		<div class="content"><!--  style="margin-top: 30px; margin-bottom: 30px; padding-bottom: 10px; background-color: #FFF;" -->
+			<div class="container">
+				<center><img src="img/BlackSquad.png" /><br><br>
+				<?php
+				if(!isset($_SESSION['steamid'])) {
+					loginbutton();
+				}  else {
+					include ('steamauth/userInfo.php');
+					include ('steamauth/userAchievements.php');
+					$level = "Level 0-30";
+					foreach($steamprofile['achievements'] as $value) {
+						if (!$value["achieved"]) continue;
+						switch ($value["apiname"]) {
+							case "A008_BlackSquad": $level = "Level 100"; break;
+							case "A006_FieldOfficer": $level = "Level 60-99"; break;
+							case "A005_HigherOfficer": $level = "Level 50-59"; break;
+							case "A004_SeniorOfficer": $level = "Level 40-49"; break;
+							case "A003_Officer": $level = "Level 30-39"; break;
+						}
+					}
+					echo '<font size="60" color="white">'.$level."</font>";
+					echo "<br><br>";
+					logoutbutton();
+				}    
+				?>
+				</center>
+			</div> 
+		<!--div class="pull-right">
 			<i><a href="https://github.com/BlackSquadGame/rank-sync">BlackSquad Rank Sync</a> by <a href="https://github.com/Bluscream">Bluscream</a>.</i>
-		</div>
-	</div> 
-  </body>
+		</div-->
+		</div> 
+	</body>
 </html>
--- steamauth/userInfo.php
+++ steamauth/userInfo.php
@@ -1,8 +1,7 @@
 <?php
 if (empty($_SESSION['steam_uptodate']) or empty($_SESSION['steam_personaname'])) {
 	require 'SteamConfig.php';
-	$url = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".$steamauth['apikey']."&steamids=".$_SESSION['steamid']);
-	// $_SESSION['steam_profile'] = $url;
+	$url = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=".$steamauth['apikey']."&steamids=".$_SESSION['steamid']);
 	$content = json_decode($url, true);
 	$dir = "cache/{$content['response']['players'][0]['steamid']}/";
 	if (!file_exists($dir)) {
@@ -11,26 +10,28 @@ if (empty($_SESSION['steam_uptodate']) or empty($_SESSION['steam_personaname']))
 	$buffer = fopen($dir."profile.json", "w+");
 	fwrite($buffer, json_encode($content, JSON_PRETTY_PRINT));
 	fclose($buffer);
-	$_SESSION['steam_steamid'] = $content['response']['players'][0]['steamid'];
-	$_SESSION['steam_communityvisibilitystate'] = $content['response']['players'][0]['communityvisibilitystate'];
-	$_SESSION['steam_profilestate'] = $content['response']['players'][0]['profilestate'];
-	$_SESSION['steam_personaname'] = $content['response']['players'][0]['personaname'];
-	$_SESSION['steam_lastlogoff'] = $content['response']['players'][0]['lastlogoff'];
-	$_SESSION['steam_profileurl'] = $content['response']['players'][0]['profileurl'];
-	$_SESSION['steam_avatar'] = $content['response']['players'][0]['avatar'];
-	$_SESSION['steam_avatarmedium'] = $content['response']['players'][0]['avatarmedium'];
-	$_SESSION['steam_avatarfull'] = $content['response']['players'][0]['avatarfull'];
-	$_SESSION['steam_personastate'] = $content['response']['players'][0]['personastate'];
-	if (isset($content['response']['players'][0]['realname'])) { 
-		   $_SESSION['steam_realname'] = $content['response']['players'][0]['realname'];
+	$user = $content['response']['players'][0];
+	$_SESSION['steam_steamid'] = $user['steamid'];
+	$_SESSION['steam_communityvisibilitystate'] = $user['communityvisibilitystate'];
+	$_SESSION['steam_profilestate'] = $user['profilestate'];
+	$_SESSION['steam_personaname'] = $user['personaname'];
+	$_SESSION['steam_lastlogoff'] = $user['lastlogoff'];
+	$_SESSION['steam_profileurl'] = $user['profileurl'];
+	$_SESSION['steam_avatar'] = $user['avatar'];
+	$_SESSION['steam_avatarmedium'] = $user['avatarmedium'];
+	$_SESSION['steam_avatarfull'] = $user['avatarfull'];
+	$_SESSION['steam_personastate'] = $user['personastate'];
+	if (isset($user['realname'])) { 
+		   $_SESSION['steam_realname'] = $user['realname'];
 	   } else {
 		   $_SESSION['steam_realname'] = "Real name not given";
 	}
-	$_SESSION['steam_primaryclanid'] = $content['response']['players'][0]['primaryclanid'];
-	$_SESSION['steam_timecreated'] = $content['response']['players'][0]['timecreated'];
+	if (isset($user['primaryclanid'])){
+		$_SESSION['steam_primaryclanid'] = $user['primaryclanid'];
+		$_SESSION['steam_timecreated'] = $user['timecreated'];
+	}
 	$_SESSION['steam_uptodate'] = time();
 }
-// $steamprofile['profile'] = $_SESSION['steam_profile'];
 $steamprofile['steamid'] = $_SESSION['steam_steamid'];
 $steamprofile['communityvisibilitystate'] = $_SESSION['steam_communityvisibilitystate'];
 $steamprofile['profilestate'] = $_SESSION['steam_profilestate'];
@@ -42,8 +43,10 @@ $steamprofile['avatarmedium'] = $_SESSION['steam_avatarmedium'];
 $steamprofile['avatarfull'] = $_SESSION['steam_avatarfull'];
 $steamprofile['personastate'] = $_SESSION['steam_personastate'];
 $steamprofile['realname'] = $_SESSION['steam_realname'];
-$steamprofile['primaryclanid'] = $_SESSION['steam_primaryclanid'];
-$steamprofile['timecreated'] = $_SESSION['steam_timecreated'];
+if (isset($_SESSION['steam_primaryclanid'])){
+	$steamprofile['primaryclanid'] = $_SESSION['steam_primaryclanid'];
+	$steamprofile['timecreated'] = $_SESSION['steam_timecreated'];
+}
 $steamprofile['uptodate'] = $_SESSION['steam_uptodate'];
 
 // Version 3.2
